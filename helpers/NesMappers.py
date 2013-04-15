06cd0e3d4792625045d6db639c27835a09edc235
jeroanan <david.tosslehoff@gmail.com>
2013-04-15 22:23:18 +0100

    Add mapper descriptions file

---
+++ helpers/NesMappers.py
@@ -0,0 +1,89 @@
+class NesMappers:
+
+    def mapperToString(self, mapperNumber):
+        """Takes a number in and gives a textual description of the mapper concerned"""
+        
+        mapperDesc = "None";
+
+        if mapperNumber=0:
+            pass
+        if mapperNumber=1:
+            pass
+        if mapperNumber=2:
+            pass
+        if mapperNumber=3:
+            pass
+        if mapperNumber=4:
+            pass
+        if mapperNumber=5:
+            pass
+        if mapperNumber=6:
+            pass
+        if mapperNumber=7:
+            pass
+        if mapperNumber=8:
+            pass
+        if mapperNumber=9:
+            pass
+        if mapperNumber=10:
+            pass
+        if mapperNumber=11:
+            pass
+        if mapperNumber=12:
+            pass
+        if mapperNumber=13:
+            pass
+        if mapperNumber=14:
+            pass
+        if mapperNumber=15:
+            pass
+        if mapperNumber=16:
+            pass
+        if mapperNumber=17:
+            pass
+        if mapperNumber=18:
+            pass
+        if mapperNumber=19:
+            pass
+        if mapperNumber=20:
+            pass
+        if mapperNumber=21:
+            pass
+        if mapperNumber=22:
+            pass
+        if mapperNumber=23:
+            pass
+        if mapperNumber=24:
+            pass
+        if mapperNumber=25:
+            pass
+        if mapperNumber=32:
+            pass
+        if mapperNumber=33:
+            pass
+        if mapperNumber=34:
+            pass
+        if mapperNumber=64:
+            pass
+        if mapperNumber=65:
+            pass
+        if mapperNumber=66:
+            pass
+        if mapperNumber=67:
+            pass
+        if mapperNumber=68:
+            pass
+        if mapperNumber=69:
+            pass
+        if mapperNumber=70:
+            pass
+        if mapperNumber=78:
+            pass
+        if mapperNumber=79:
+            pass
+        if mapperNumber=81:
+            pass
+        if mapperNumber=91:
+            pass
+        
+        return mapperDesc
