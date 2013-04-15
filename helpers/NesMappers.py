a24d269824faec79e9bcee416e85d966cdf9d27a
jeroanan <david.tosslehoff@gmail.com>
2013-04-15 22:34:35 +0100

    Fleshed out NesMappers.

--- helpers/NesMappers.py
+++ helpers/NesMappers.py
@@ -1,89 +1,86 @@
 class NesMappers:
 
-    def mapperToString(self, mapperNumber):
+    @staticmethod
+    def mapperToString(mapperNumber):
         """Takes a number in and gives a textual description of the mapper concerned"""
         
         mapperDesc = "None";
 
-        if mapperNumber=0:
-            pass
-        if mapperNumber=1:
-            pass
-        if mapperNumber=2:
-            pass
-        if mapperNumber=3:
-            pass
-        if mapperNumber=4:
-            pass
-        if mapperNumber=5:
-            pass
-        if mapperNumber=6:
-            pass
-        if mapperNumber=7:
-            pass
-        if mapperNumber=8:
-            pass
-        if mapperNumber=9:
-            pass
-        if mapperNumber=10:
-            pass
-        if mapperNumber=11:
-            pass
-        if mapperNumber=12:
-            pass
-        if mapperNumber=13:
-            pass
-        if mapperNumber=14:
-            pass
-        if mapperNumber=15:
-            pass
-        if mapperNumber=16:
-            pass
-        if mapperNumber=17:
-            pass
-        if mapperNumber=18:
-            pass
-        if mapperNumber=19:
-            pass
-        if mapperNumber=20:
-            pass
-        if mapperNumber=21:
-            pass
-        if mapperNumber=22:
-            pass
-        if mapperNumber=23:
-            pass
-        if mapperNumber=24:
-            pass
-        if mapperNumber=25:
-            pass
-        if mapperNumber=32:
-            pass
-        if mapperNumber=33:
-            pass
-        if mapperNumber=34:
-            pass
-        if mapperNumber=64:
-            pass
-        if mapperNumber=65:
-            pass
-        if mapperNumber=66:
-            pass
-        if mapperNumber=67:
-            pass
-        if mapperNumber=68:
-            pass
-        if mapperNumber=69:
-            pass
-        if mapperNumber=70:
-            pass
-        if mapperNumber=78:
-            pass
-        if mapperNumber=79:
-            pass
-        if mapperNumber=81:
-            pass
-        if mapperNumber=91:
-            pass
-        
+        if mapperNumber==1: 
+            mapperDesc="Nintendo MMC1"
+        if mapperNumber==2: 
+            mapperDesc="CNROM switch"
+        if mapperNumber== 3: 
+            mapperDesc="UNROM switch"
+        if mapperNumber==4: 
+            mapperDesc="Nintendo MMC3"
+        if mapperNumber==5: 
+            mapperDesc="Nintendo MMC5"
+        if mapperNumber==6: 
+            mapperDesc="FFE F4xxx"
+        if mapperNumber==7: 
+            mapperDesc="AOROM switch"
+        if mapperNumber==8: 
+            mapperDesc="FFE F3xxx"
+        if mapperNumber==9: 
+            mapperDesc="Nintendo MMC2"
+        if mapperNumber==10: 
+            mapperDesc="Nintendo MMC4"
+        if mapperNumber==11: 
+            mapperDesc="ColorDreams chip"
+        if mapperNumber==12: 
+            mapperDesc="- FFE F6xxx"
+        if mapperNumber==13: 
+            mapperDesc="CPROM switch"
+        if mapperNumber==15: 
+            mapperDesc="100-in-1 switch"
+        if mapperNumber==16: 
+            mapperDesc="Bandai chip"
+        if mapperNumber==17: 
+            mapperDesc="FFE F8xxx"
+        if mapperNumber==18: 
+            mapperDesc="Jaleco SS8806 chip"
+        if mapperNumber==19: 
+            mapperDesc="Namcot 106 chip"
+        if mapperNumber==20: 
+            mapperDesc="Nintendo DiskSystem"
+        if mapperNumber==21: 
+            mapperDesc="Konami VRC4a"
+        if mapperNumber==22: 
+            mapperDesc="Konami VRC2a"
+        if mapperNumber==23: 
+            mapperDesc="Konami VRC2a"
+        if mapperNumber==24: 
+            mapperDesc="Konami VRC6"
+        if mapperNumber==25: 
+            mapperDesc="Konami VRC4b"
+        if mapperNumber==32: 
+            mapperDesc="Irem G-101 chip"
+        if mapperNumber==33: 
+            mapperDesc="Taito TC0190/TC0350"
+        if mapperNumber==34: 
+            mapperDesc="Nina-1 board"
+        if mapperNumber==64: 
+            mapperDesc="Tengen RAMBO-1 chip"
+        if mapperNumber==65: 
+            mapperDesc="Irem H-3001 chip"
+        if mapperNumber==66: 
+            mapperDesc="GNROM switch"
+        if mapperNumber==67: 
+            mapperDesc="SunSoft3 chip"
+        if mapperNumber==68: 
+            mapperDesc="SunSoft4 chip"
+        if mapperNumber==69: 
+            mapperDesc="SunSoft5 FME-7 chip"
+        if mapperNumber==71: 
+            mapperDesc="Camerica chip"
+        if mapperNumber==78: 
+            mapperDesc="Irem 74HC161Irem 74HC161/32-based]]"
+        if mapperNumber==79: 
+            mapperDesc="AVE Nina-3 board"
+        if mapperNumber==81: 
+            mapperDesc="AVE Nina-6 board"
+        if mapperNumber==91: 
+            mapperDesc="Pirate HK-SF3 chip "
+      
         return mapperDesc
